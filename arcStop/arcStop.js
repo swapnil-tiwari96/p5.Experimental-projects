40b6c4a612281eba43eb13aa62b62e2811c49de8
Swapnil Tiwari <swaptiwari@outlook.com>
2021-11-14 23:32:26 +0530

    changes

--- arcStop/arcStop.js
+++ arcStop/arcStop.js
@@ -1,7 +1,5 @@
 function preload() {
-  song = loadSound(
-    "Skrillex - First Of The Year (Equinox) [Official Audio].mp3"
-  );
+  song = loadSound("FLP(VDO) AUDIO 14.wav");
 }
 
 function keyPressed() {
@@ -9,24 +7,24 @@ function keyPressed() {
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fft = new p5.FFT(0.9, 128);
-  arc1 = new manyArcs(windowWidth / 2, windowHeight / 2);
-  // arc2 = new manyArcs(windowWidth / 1.2, windowHeight / 6);
-  // arc3 = new manyArcs(windowWidth / 6, windowHeight / 1.2);
-  // arc4 = new manyArcs(windowWidth / 6, windowHeight / 6);
-  // arc5 = new manyArcs(windowWidth / 2, windowHeight / 6);
-  // arc6 = new manyArcs(windowWidth / 2, windowHeight / 1.2);
+  fft = new p5.FFT(0.8, 128);
+  arc1 = new manyArcs(windowWidth / 1.2, windowHeight / 1.2 - 40);
+  arc2 = new manyArcs(windowWidth / 1.2, windowHeight / 6 + 40);
+  arc3 = new manyArcs(windowWidth / 6, windowHeight / 1.2 - 40);
+  arc4 = new manyArcs(windowWidth / 6, windowHeight / 6 + 40);
+  arc5 = new manyArcs(windowWidth / 2, windowHeight / 6 + 40);
+  arc6 = new manyArcs(windowWidth / 2, windowHeight / 1.2 - 40);
 }
 
 function draw() {
   let spectrum = fft.analyze();
-  background(255);
+  background(0);
   arc1.createObjects(1, 2, 3, 4, 5);
-  //   arc2.createObjects(5, 4, 3, 2, 1);
-  //   arc3.createObjects(1, 3, 5, 2, 4);
-  //   arc4.createObjects(2, 4, 1, 3, 5);
-  //   arc5.createObjects(1, 5, 2, 4, 3);
-  //   arc6.createObjects(3, 4, 2, 5, 1);
+  arc2.createObjects(5, 4, 3, 2, 1);
+  arc3.createObjects(1, 3, 5, 2, 4);
+  arc4.createObjects(2, 4, 1, 3, 5);
+  arc5.createObjects(1, 5, 2, 4, 3);
+  arc6.createObjects(3, 4, 2, 5, 1);
 }
 
 class oneArc {
@@ -40,7 +38,7 @@ class oneArc {
   createArc(_stop, _sW, _r, _g, _b) {
     strokeWeight(_sW);
     stroke(_r, _g, _b);
-    fill(0);
+    noFill();
     arc(this.x, this.y, this.w, this.h, 0, PI + _stop, PIE);
   }
 }
@@ -51,35 +49,35 @@ class manyArcs {
     this.y = _y;
   }
   createObjects(a, b, c, d, e) {
-    let circle5 = new oneArc(this.x, this.y, 150 * e, 150 * e).createArc(
-      fft.getEnergy("treble") / 90,
+    let circle5 = new oneArc(this.x, this.y, 60 * e, 60 * e).createArc(
+      fft.getEnergy("treble") / 70,
       10,
       136,
       224,
       239
     );
-    let circle4 = new oneArc(this.x, this.y, 150 * d, 150 * d).createArc(
-      fft.getEnergy("highMid") / 100,
+    let circle4 = new oneArc(this.x, this.y, 60 * d, 60 * d).createArc(
+      fft.getEnergy("highMid") / 70,
       8,
       185,
       22,
       70
     );
-    let circle3 = new oneArc(this.x, this.y, 150 * c, 150 * c).createArc(
+    let circle3 = new oneArc(this.x, this.y, 60 * c, 60 * c).createArc(
       fft.getEnergy("mid") / 80,
       6,
       47,
       221,
       146
     );
-    let circle2 = new oneArc(this.x, this.y, 150 * b, 150 * b).createArc(
+    let circle2 = new oneArc(this.x, this.y, 60 * b, 60 * b).createArc(
       fft.getEnergy("lowMid") / 90,
       4,
       242,
       240,
       19
     );
-    let circle1 = new oneArc(this.x, this.y, 150 * a, 150 * a).createArc(
+    let circle1 = new oneArc(this.x, this.y, 60 * a, 60 * a).createArc(
       fft.getEnergy("bass") / 90,
       2,
       47,
---
+++ starCircles/expandingCircles.js
@@ -0,0 +1,56 @@
+//Swapnil Tiwari
+//Expanding circles according the the frequencies.
+
+let circles = [];
+let circles1 = [];
+function preload() {
+  song = loadSound("FLP(VDO) AUDIO 21.wav"); //load sound
+}
+
+function keyPressed() {
+  song.play();
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fft = new p5.FFT(0.9, 128);
+  //make circles array
+  for (let i = 0; i < 2000; i++) {
+    circles[i] = new objCircle(random(width), random(height));
+  }
+}
+
+function draw() {
+  background(119, 228, 212);
+  for (let i = 1500; i < 2000; i++) {
+    circles[i].makeCircle(15, "mid", 50, 193, 205);
+  }
+  for (let i = 1000; i < 1500; i++) {
+    circles[i].makeCircle(05, "bass", 84, 65, 121);
+  }
+  for (let i = 0; i < 500; i++) {
+    circles[i].makeCircle(20, "treble", 23, 215, 160);
+  }
+  for (let i = 500; i < 1000; i++) {
+    circles[i].makeCircle(10, "lowMid", 251, 244, 109);
+  }
+}
+
+class objCircle {
+  constructor(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+  }
+
+  makeCircle(r, mode, r1, g, b) {
+    let spectrum = fft.analyze();
+    fill(r1, g, b);
+    //noStroke();
+    strokeWeight(1);
+    stroke(185, 22, 70);
+    circle(
+      this.x,
+      this.y,
+      r + map(fft.getEnergy(`${mode}`) / 2, 0, 64, r, r + 10)
+    );
+  }
+}
